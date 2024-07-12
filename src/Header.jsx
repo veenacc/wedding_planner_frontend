4deebc7e7160a8fb020849ec3c80dde1617182af
veenacc <veena.chenicheri@gmail.com>
2024-07-12 12:36:45 -0700

    added conditional rendering for signup,login,logout links

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,8 +6,10 @@ export function Header() {
     <header>
       <nav>
         {/* <a href="/Home">Home</a> | <a href="#">Link</a> */}
-        <Link to="/Home">Home</Link>  |  <Link to="/Signup">Signup</Link>  | 
-          |  <Link to="/Login">Login</Link>   |   <Link to="/LogoutLink">Logout</Link>
+        <Link to="/Home">Home</Link>  |  
+        {!localStorage.getItem('jwt')  && <Link to="/Signup">Signup</Link>}  | 
+          |  {!localStorage.getItem('jwt')  && <Link to="/Login">Login</Link>}  
+          |   {localStorage.getItem('jwt') && <Link to="/LogoutLink">Logout</Link>}
         
         
       </nav>
