68e03b27e76b4ce4847b11feaf21df817eee3690
veenacc <veena.chenicheri@gmail.com>
2024-07-19 13:49:22 -0400

    styled create new events and changed datetime input type to datetime-local in form

--- src/EventsNew.jsx
+++ src/EventsNew.jsx
@@ -11,28 +11,30 @@ export function EventsNew(props) {
     
   }
   return (
-    <div>
-      <h1>Create New Event</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
+    <div className="bg-cover bg-center bg-orange-50 h-dvh w-full">
+      <h1 className="text-center p-8 font-serif text-4xl mb-4">Create New Event</h1>
+      <form onSubmit={handleSubmit} className="grid grid-cols-2 gap-6 place-content-evenly h-48"> 
+        <div className="flex items-center justify-center p-3">
           Name: <input name="name" type="text" />
         </div>
-        <div>
+        <div className="flex items-center justify-center p-3">
           Description: <input name ="description" type="text" />
         </div>
-        <div>
+        <div className="flex items-center justify-center p-3">
           Address: <input name="address" type="text" />
         </div>
-        <div>
+        <div className="flex items-center justify-center p-3">
           Image Url: <input name="image_url" type="url" />
         </div>
-        <div>
-          Start time: <input name="start_time" type="datetime" />
+        <div className="flex items-center justify-center p-3">
+          Start time: <input name="start_time" type="datetime-local" />
         </div>
-        <div>
-          End time: <input name="end_time" type="datetime" />
+        <div className="flex items-center justify-center p-3">
+          End time: <input name="end_time" type="datetime-local" />
         </div>
-        <button type="submit">Create Item</button>
+        <div className="grid grid-rows-1  justify-around">
+          <button type="submit" className="p-3 hover:text-orange-900 bg-orange-300 rounded-md outline outline-offset-2 outline-amber-900 ">Create Item</button>
+        </div>
       </form>
     </div>
   );
